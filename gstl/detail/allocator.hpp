5e5782e01145518c67af764e8d5d76dd300083cb
Anton Matosov <anton.matosov@gmail.com>
2008-06-22 14:06:59 +0000

    implemented allocator class

--- gstl/detail/allocator.hpp
+++ gstl/detail/allocator.hpp
@@ -29,7 +29,109 @@ Please feel free to contact me via e-mail: shikin@users.sourceforge.net
 
 namespace gstl
 {
+template< class T >
+	class allocator
+	{
+	public:
+		typedef T									value_type;
+		typedef allocator< value_type >				self_type;
 
+		typedef value_type*								pointer;
+		typedef const value_type*						const_pointer;
+		typedef value_type&								reference;
+		typedef const value_type&						const_reference;
+		
+		typedef size_t									size_type;
+		typedef ptrdiff_t								difference_type;
+
+		template<class Other>
+		struct rebind
+		{	// convert an allocator<T> to an allocator <Other>
+			typedef typename allocator< Other > other;
+		};
+
+		// return address of mutable val
+		inline pointer address( reference val ) const
+		{	
+			return pointer(&val);
+		}
+
+		// return address of nonmutable val
+		inline const_pointer address( const_reference val ) const
+		{	
+			return const_pointer(&val);
+		}
+
+		// construct default allocator (do nothing)
+		inline allocator()
+		{
+		}
+
+		template<class other>
+		inline allocator( const allocator<other>& ) /*throw()*/
+		{	// construct from a related allocator (do nothing)
+		}
+
+		template<class other>
+		inline self_type& operator=( const allocator<other>& )
+		{	// assign from a related allocator (do nothing)
+			return (*this);
+		}
+
+		// deallocate object at ptr, ignore size
+		inline void deallocate( pointer ptr, size_type /*size*/ )
+		{	
+			::operator delete( ptr );
+		}
+
+		// allocate array of count elements
+		inline pointer allocate(size_type count)
+		{	
+			return static_cast<pointer>( ::operator new( count * sizeof(value_type) ) );
+		}
+
+		// allocate array of count elements, ignore hint
+		inline pointer allocate(size_type count, const void *)
+		{	
+			return (allocate(count));
+		}
+
+		// construct object at ptr with value val
+		inline void construct(pointer ptr, const_reference val)
+		{	
+			::new (&*ptr) value_type(val);
+		}
+
+		// destroy object at ptr
+		inline void destroy(pointer ptr)
+		{	
+			ptr;//VS 2008 warning
+			(&*ptr)->~value_type();
+		}
+
+		// estimate maximum array size
+		inline size_type max_size() const 
+		{	
+			size_type count = ~size_type(0) / sizeof value_type;
+			return (0 < count ? count : 1);
+		}
+
+	};
+
+	// allocator TEMPLATE OPERATORS
+	template<class T,
+	class U>
+	inline bool operator==(const allocator<T>&, const allocator<U>&) /*throw()*/
+	{	// test for allocator equality (always true)
+		return true;
+	}
+
+	template<class T,
+	class U>
+	inline bool operator!=(const allocator<T>&, const allocator<U>&) /*throw()*/
+	{	// test for allocator inequality (always false)
+		return false;
+	}
 }
 
 #endif GSTL_ALLOCATOR_HEADER
