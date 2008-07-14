1a5a884bb9c8ca822c74f94ae18d75c6d3de0ace
Anton Matosov <anton.matosov@gmail.com>
2008-07-14 19:10:39 +0000

    implemented char_traits according to C++ std 2003

--- gstl/detail/char_traits.hpp
+++ gstl/detail/char_traits.hpp
@@ -30,10 +30,136 @@ Please feel free to contact me via e-mail: shikin@users.sourceforge.net
 
 namespace gstl
 {
-	template<class T>
-	class char_traits
-	{
+	typedef void streamoff;
+	typedef void streampos;
+	typedef void mbstate_t;
 
+	template<class T>
+	struct char_traits
+	{
+		typedef T			char_type;
+		typedef char		char_type;
+		typedef int			int_type;
+		typedef streamoff	off_type;
+		typedef streampos	pos_type;
+		typedef mbstate_t	state_type;
+		
+		static inline void assign( char_type& c1, const char_type& c2 )
+		{
+			c1 = c2;
+		}
+		
+		static inline bool eq( const char_type& c1, const char_type& c2 )
+		{
+			return c1 == c2;
+		}
+		
+		static inline bool lt( const char_type& c1, const char_type& c2 )
+		{
+			return c1 < c2;
+		}
+
+		static inline int compare( const char_type* s1, const char_type* s2, size_t n )
+		{
+			assert( s1 != 0 && "s1 is null" );
+			assert( s2 != 0 && "s2 is null" );
+			while( n-- )
+			{
+				if( !eq( *++s1, *++s2 ) )
+				{
+					if( lt( *s1, *s2 ) )
+					{
+						return -1;
+					}
+					else
+					{
+						return 1;
+					}
+				}
+			}
+			return 0;
+		}
+
+		static inline size_t length( const char_type* s )
+		{
+			assert( s != 0 && "s is null" );
+			size_t len = 0;
+			while( !eq( *s, char_type() ) )
+			{
+				++len;
+			}
+			return len;
+		}
+
+		static inline const char_type* find( const char_type* s, size_t n, const char_type& a )
+		{
+			assert( s != 0 && "s is null" );
+			while( n-- )
+			{
+				if( eq( *++s, a ) )
+				{
+					return s;
+				}
+			}
+			return 0;
+		}
+
+		static inline char_type* move( char_type* s1, const char_type* s2, size_t n )
+		{
+			char_type* s = s1;
+			while( n-- )
+			{
+				assign( *++s1, *++s2 );
+			}
+			return s;
+		}
+
+		static inline char_type* copy( char_type* s1, const char_type* s2, size_t n )
+		{
+			return move( s1, s2, n )
+		}
+
+		static inline char_type* assign( char_type* s, size_t n, char_type a )
+		{
+			char_type* s = s;
+			while( n-- )
+			{
+				assign( *++s, a );
+			}
+			return s;
+		}
+
+		static inline int_type not_eof( const int_type& c )
+		{
+			if( eq_int_type( c, eof() ) )
+			{
+				return c;
+			}
+			else
+			{
+				return ~eof();
+			}
+		}
+
+		static inline char_type to_char_type( const int_type& c )
+		{
+			return static_cast<char_type>( c );
+		}
+
+		static inline int_type to_int_type( const char_type& c )
+		{
+			return c;
+		}
+
+		static inline bool eq_int_type( const int_type& c1, const int_type& c2 )
+		{
+			return c1 == c2;
+		}
+
+		static inline int_type eof()
+		{
+			return to_int_type( EOF );
+		}
 	};
 }
 
--- gstl/detail/string.hpp
+++ gstl/detail/string.hpp
@@ -37,8 +37,21 @@ namespace gstl
 	class basic_string
 	{
 	public:
-		basic_string();
+		typedef CharT							char_type;
+		typedef Traits							char_traits_type;
+		typedef Alloc::rebind<char_type>		allocator_type;
 
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+
+		typedef typename allocator_type::size_type			size_type;
+		typedef typename allocator_type::difference_type	difference_type;
+
+		basic_string();
+		basic_string( pointer str );
+		basic_string( pointer str, size_type length );
 
 	};
 }
