ef6279b7aa8e23e0b4bec71fe05caefcef9ab85e
Anton Matosov <anton.matosov@gmail.com>
2008-08-31 15:48:14 +0000

    added more boost files

--- boost/collect boost sources.bat
+++ boost/collect boost sources.bat
@@ -4,9 +4,10 @@ setlocal ENABLEEXTENSIONS
 set file_to_check=%cd%\..\detail\iterator.hpp
 set boost_path=C:\Boost\include\boost-1_35
 
-bcp --boost=%boost_path% --scan "%file_to_check%" "%cd%"
+::bcp --boost=%boost_path% --scan "%file_to_check%" "%cd%"
 
-bcp --boost=%boost_path% bind test mpl tuple concept_check config "%cd%"
+::bcp --boost=%boost_path% bind test mpl tuple concept_check config type_traits "%cd%"
+bcp --boost=%boost_path% iterator "%cd%"
 
 
 pause
