b8068a9955e77aa92c5d2ed50903a6104b5e72be
Anton Matosov <anton.matosov@gmail.com>
2008-09-06 19:28:18 +0000

    implemented unit tests for for_each, find and find_if algorithms

--- unit_tests/test_algorithm.cpp
+++ unit_tests/test_algorithm.cpp
@@ -32,19 +32,54 @@ namespace
 	size_t array_len = GSTL_ARRAY_LEN( array, int );
 	int *array_end = array + array_len;
 	int array_item = array[ array_len / 2 ];
+
+
+	typedef std::vector<int> int_vec_type;
+	typedef const int_vec_type const_int_vec_type;
+
+
+	int_vec_type int_vec( array, array_end );
+	const_int_vec_type const_int_vec( array, array_end );
+
+
 }
 
 class algorithm_test_fixture
 {
 public:
-	typedef std::vector<int> int_vec_type;
-	int_vec_type vec_;
 	
 	algorithm_test_fixture()
-		:vec_( array, array_end )
 	{
 
 	}
+
+	static bool eq_arr_item( int val )
+	{
+		return val == array_item;
+	}
+
+	template<class InputIterator>
+	class for_each_verifier
+	{
+		typedef InputIterator iterator_type;
+		typedef typename gstl::iterator_traits<iterator_type>::value_type value_type;
+
+		
+
+		iterator_type first_;
+
+	public:
+		for_each_verifier( iterator_type first )
+			:first_( first )
+		{
+
+		}
+		void operator()( value_type val )
+		{
+			BOOST_CHECK_EQUAL( *first_, val );
+			++first_;
+		}
+	};
 };
 
 
@@ -52,9 +87,29 @@ BOOST_FIXTURE_TEST_SUITE( algorithm_test, algorithm_test_fixture );
 
 typedef boost::mpl::list< int, float, double > t_list;
 
+BOOST_AUTO_TEST_CASE( test_for_each )
+{
+	for_each_verifier<int_vec_type::iterator> ver1( int_vec.begin() );
+	gstl::for_each( int_vec.begin(), int_vec.end(), ver1 );
+
+
+	for_each_verifier<int_vec_type::const_iterator> ver2( const_int_vec.begin() );
+	gstl::for_each( const_int_vec.begin(), const_int_vec.end(), ver2 );
+}
+
 BOOST_AUTO_TEST_CASE( test_non_modifying )
 {
-	gstl::find( vec_.begin(), vec_.end(), array_item );
+	int_vec_type::iterator fres1 = gstl::find( int_vec.begin(), int_vec.end(), array_item );
+	BOOST_CHECK_EQUAL( *fres1, array_item );
+	int_vec_type::const_iterator fres2 = gstl::find( const_int_vec.begin(), const_int_vec.end(), array_item );
+	BOOST_CHECK_EQUAL( *fres2, array_item );
+
+ 	int_vec_type::iterator fres3 = gstl::find_if( int_vec.begin(), int_vec.end(), eq_arr_item );
+ 	BOOST_CHECK_EQUAL( *fres3, array_item );
+ 	int_vec_type::const_iterator fres4 = gstl::find_if( const_int_vec.begin(), const_int_vec.end(), eq_arr_item );
+ 	BOOST_CHECK_EQUAL( *fres4, array_item );
+
+	//std::find_end()
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE( test_1, type, t_list )
