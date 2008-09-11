3d1fabe2b6c47f3cbb16ed0488d5664e69824c12
Anton Matosov <anton.matosov@gmail.com>
2008-09-11 20:25:25 +0000

    added boost folder (part1)

---
+++ boost/collect boost sources.bat
@@ -0,0 +1,12 @@
+::echo off
+setlocal ENABLEEXTENSIONS 
+
+set file_to_check=%cd%\..\memory-mgr\segment_manager.h
+set boost_path=C:\Boost\include\boost-1_35
+
+::bcp --boost=%boost_path% test tuple "%cd%"
+bcp --boost=%boost_path% --scan "%file_to_check%" "%cd%"
+
+
+pause
+endlocal
