cadf155948e7535764ad70bda84ad3d3aed86245
Anton Matosov <anton.matosov@gmail.com>
2008-09-12 04:55:20 +0000

    added required boost libraries (part3)

--- boost/collect boost sources.bat
+++ boost/collect boost sources.bat
@@ -4,8 +4,8 @@ setlocal ENABLEEXTENSIONS
 set file_to_check=%cd%\..\memory-mgr\segment_manager.h
 set boost_path=C:\Boost\include\boost-1_35
 
-::bcp --boost=%boost_path% test tuple "%cd%"
-bcp --boost=%boost_path% --scan "%file_to_check%" "%cd%"
+bcp --boost=%boost_path% test utility "%cd%"
+::bcp --boost=%boost_path% --scan "%file_to_check%" "%cd%"
 
 
 pause
