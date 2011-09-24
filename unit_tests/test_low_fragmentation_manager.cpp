edf580831318827716764e857eee0591c1387bbc
Anton Matosov <anton.matosov@gmail.com>
2011-09-24 20:32:31 +0000

    left only function that calculates pool ids

--- unit_tests/test_low_fragmentation_manager.cpp
+++ unit_tests/test_low_fragmentation_manager.cpp
@@ -47,55 +47,9 @@ typedef memory_mgr::low_fragmentation_manager
 
 MGR_DECLARE_MANAGER_CLASS( low_fragmentation_mgr, lfm_mgr );
 
-static size_t get_allocation_size( size_t requested_size, size_t& poolId )
-{
-	const size_t allocation_segments[] = { 256, 512, 1024, 2048, 4096, 8196, 16384 };
-	size_t resulting_size = requested_size;
-	poolId = 0;
-
-	for( int segment = 0; segment < _countof( allocation_segments ); ++segment )
-	{
-		if( requested_size <= allocation_segments[segment] )
-		{
-			const int multiple = allocation_segments[segment] / 32;
-
-			size_t sizeLeft = (requested_size + multiple - 1);
-			int negativeMultiple = (-multiple);
-			resulting_size = sizeLeft & negativeMultiple;
-			poolId = resulting_size / multiple;
-
- 			std::cout << std::hex
- 				<< requested_size
- 				<< ' ' << allocation_segments[segment]
- 				<< ' ' << multiple
- 				<< ' ' << sizeLeft
- 				<< ' ' << negativeMultiple 
- 				<< ' ' << resulting_size 
- 				<< ' ' << poolId 
-				<< ' ' << sizeLeft / multiple
-				<< ' ' << (sizeLeft / multiple) * (segment + 1)
- 				<< '\n';
-
-			assert( poolId == (sizeLeft / multiple) );
-			break;
-		}
-	}
-	return resulting_size;
-}
-
-struct SegmentInfo
-{
-	size_t maxBlockSize;
-	size_t multiple;
-	size_t numPools;
-	size_t firstPoolId;
-};
-
-size_t *poolIds;
-
 BOOST_AUTO_TEST_SUITE( test_low_fragmentation_manager )
 
-	BOOST_AUTO_TEST_CASE( calculate_pools_sizes_and_count )
+	BOOST_AUTO_TEST_CASE( calculate_pools_ids )
 	{
 		const size_t allocation_segments[] = { 256, 512, 1024, 2048, 4096, 8196, 16384 };
 
@@ -107,28 +61,21 @@ BOOST_AUTO_TEST_SUITE( test_low_fragmentation_manager )
 		size_t itemsPrinted = 0;
 		{
 			size_t prevSize = 0;
-			size_t poolId = 0;
-			poolIds = new size_t[16384];
+			int poolId = 0;
+			size_t *poolIds = new size_t[16384];
 
 			//for index 0
 			for( size_t i = 0; i < 16384; i += 1 )
 			{
-				for( int segment = 0; segment < _countof( allocation_segments ); ++segment )
+				size_t resulting_size = memory_mgr::detail::get_allocation_size( i );
+
+				if( resulting_size != prevSize )
 				{
-					if( i <= allocation_segments[segment] )
+					if( prevSize != 0 )
 					{
-						const int multiple = allocation_segments[segment] / 32;
-						size_t resulting_size = memory_mgr::detail::make_multiple_of( i, multiple );
-						if( resulting_size != prevSize )
-						{
-							if( prevSize != 0 )
-							{
-								++poolId;
-							}
-							prevSize = resulting_size;
-						}
-						break;
+						++poolId;
 					}
+					prevSize = resulting_size;
 				}
 
 				pool_by_size << poolId << ", ";
@@ -142,79 +89,6 @@ BOOST_AUTO_TEST_SUITE( test_low_fragmentation_manager )
 			}
 		}
 
-		size_t totalPoolsRequired = 0;
-		size_t prevSegmentSize = 0;
-		std::cout << "\ninSegmentSize\tMultiple\tNum pools required\n";
-		for( int segment = 0; segment < _countof( allocation_segments ); ++segment )
-		{
-			size_t segmentSize = allocation_segments[segment];
-			const int multiple = segmentSize / 32;
-			
-			size_t inSegmentSize = segmentSize - prevSegmentSize;
-
-			size_t poolsRequired = inSegmentSize / multiple;
-			std::cout << inSegmentSize << '\t' << multiple << '\t'<< poolsRequired << '\n';
-
-			prevSegmentSize = segmentSize;
-			totalPoolsRequired += poolsRequired;
-		}
-
-		std::cout << "Total pools required: " << totalPoolsRequired << '\n';
-
-		size_t prevSize = 0;
-		for( int i = 0; i < 16384; i += 1 )
-		{
-			size_t poolId = 0;
-			size_t requested_size = i;
-			const size_t allocation_segments[] = { 256, 512, 1024, 2048, 4096, 8196, 16384 };
-			size_t resulting_size = requested_size;
-
-			for( int segment = 0; segment < _countof( allocation_segments ); ++segment )
-			{
-				if( requested_size <= allocation_segments[segment] )
-				{
-					const int multiple = allocation_segments[segment] / 32;
-
-					size_t sizeLeft = (requested_size + multiple - 1);
-					int negativeMultiple = (-multiple);
-					resulting_size = sizeLeft & negativeMultiple;
-					poolId = resulting_size / multiple;
-
-					//if( resulting_size != prevSize )
-					{
-					//	std::cout //<< std::hex
-//							<< requested_size
-// 							<< ' ' << allocation_segments[segment]
-// 							<< ' ' << multiple
-// 							<< ' ' << sizeLeft
-// 							<< ' ' << negativeMultiple 
-// 							<< ' ' << resulting_size 
-// 							<< ' ' << poolId 
-// 							<< ' ' << sizeLeft / multiple
-					//		<< ' ' << (sizeLeft / multiple) * (segment + 1)
-					//		<< '\n';
-
-					}
-
-					assert( poolId == (sizeLeft / multiple) );
-
-					prevSize = resulting_size;
-					break;
-				}
-			}
-
-
-
-
-			//size_t currSize = get_allocation_size(i, poolId);
-
-//			std::cout << i << '\t' << poolId << '\n';
-// 			if( currSize != prevSize )
-// 			{
-// 				std::cout << currSize << '\t' << poolId << '\n';
-// 				prevSize = currSize;
-// 			}
-		}
 	}
 
 BOOST_AUTO_TEST_SUITE_END()
