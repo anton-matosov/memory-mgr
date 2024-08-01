5972a705002d1097ede35ed4c4d0fe20b7cdaa1a
Anton Matosov <anton.matosov@gmail.com>
2024-07-31 22:40:01 -0700

    cleanup

--- import_scripts/svn_mirror.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-svnadmin create svn-mirror
-
-echo '#!/bin/sh' > `PWD`/svn-mirror/hooks/pre-revprop-change && chmod +x svn-mirror/hooks/pre-revprop-change
-# do the actual sync
-svnsync init file://`PWD`/svn-mirror https://svn.code.sf.net/p/genericstl/code --username=shikin
-svnsync sync file://`PWD`/svn-mirror
