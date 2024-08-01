e253eb03cc7c97758be05161f49fe23d86bea3de
Anton Matosov <anton.matosov@gmail.com>
2024-07-31 22:37:11 -0700

    Remove copy of memory-mgr and submodule spec

--- memory-mgr/import_scripts/extract-author.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-authors=$(svn log -q $@ | grep -e '^r[0-9]' | awk 'BEGIN { FS = "|" } ; { print $2 } $1 ~ /r([0-9]+000)/ { print "fetched revision " substr($1, 2) > "/dev/stderr" }' | sort | uniq)
-for author in ${authors}; do
-echo "${author} = ${author}_name <${author}_mail@${author}_domain.com>";
-done
--- memory-mgr/postprocess_tools/memory_mgr_preprocess.cpp
+++
@@ -1,6 +0,0 @@
-//Added to have ability to modify include paths only
-// 
-#include "stdafx.h"
-
-// #include <memory-mgr/detail/new_proxy_operator_pp.h>
-// #include <memory-mgr/smart_ptr/smart_ptr/detail/make_shared_preprocessor.hpp>
