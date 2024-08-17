cb31a895291488bffb39ab57bb19f9df74d1fdb5
Anton Matosov <anton.matosov@gmail.com>
2024-08-16 18:16:27 -0700

    enable elevated privileges for memory-mgr tests because they create shared segment in global namespace

---
+++ cmake-modules/UAC.cmake
@@ -0,0 +1,16 @@
+
+
+function (add_elevated_manifest TARGET)
+    if (NOT WIN32)
+        return()
+    endif()
+    
+    # Path to output the manifest file relative to the build directory
+    set(MANIFEST_FILE "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.exe.manifest")
+
+    # Embed the manifest into the executable
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/elevated.manifest ${MANIFEST_FILE}
+        COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_CURRENT_BINARY_DIR} mt.exe -manifest ${MANIFEST_FILE} -outputresource:$<TARGET_FILE:${TARGET}>;#1
+    )
+endfunction()
