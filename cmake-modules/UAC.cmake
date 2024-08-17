98cd509d79d6dd3b6ac7026e82dff265fa161a2f
Anton Matosov <anton.matosov@gmail.com>
2024-08-16 22:58:17 -0700

    remove bad comment

--- cmake-modules/UAC.cmake
+++ cmake-modules/UAC.cmake
@@ -11,6 +11,6 @@ function (add_elevated_manifest TARGET)
     # Embed the manifest into the executable
     add_custom_command(TARGET ${TARGET} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/elevated.manifest ${MANIFEST_FILE}
-        COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_CURRENT_BINARY_DIR} mt.exe -manifest ${MANIFEST_FILE} -outputresource:$<TARGET_FILE:${TARGET}>;#1
+        COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_CURRENT_BINARY_DIR} mt.exe -manifest ${MANIFEST_FILE} -outputresource:$<TARGET_FILE:${TARGET}>
     )
 endfunction()
